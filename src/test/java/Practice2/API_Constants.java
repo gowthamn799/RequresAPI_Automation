c68b5c2dba6144b298aef9c0d4111f0f07f43c5f
GowthamN007 <gowthamn14339@gmail.com>
2026-05-03 18:56:58 +0530

    Practice 2 code is added

---
+++ src/test/java/Practice2/API_Constants.java
@@ -0,0 +1,33 @@
+package Practice2;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
+
+public class API_Constants extends FileReader{
+
+    public static String BaseURI=configReader("baseUri");
+    public static String ContentType=configReader("contentType");
+    public static String HeaderKey=configReader("headerKey");
+    public static String HeaderValue=configReader("headerValue");
+    public static String PostEndPoint=configReader("postEndpoint");
+    public static String PutEndPoint=configReader("putEndpoint");
+    public static String PatchEndPoint=configReader("patchEndpoint");
+    public static String GetEndPoint=configReader("getEndPoint");
+    public static String DeleteEndPoint=configReader("deleteEndpoint");
+
+
+    public static RequestSpecification requestSpec(){
+        RequestSpecification request= given().baseUri(BaseURI).contentType(ContentType).header(HeaderKey,HeaderValue).log().all();
+        return request;
+    }
+    public static ResponseSpecification responseSpec(int status,long time,String key,String value){
+        ResponseSpecification response= new ResponseSpecBuilder().expectStatusCode(status).expectResponseTime(lessThan(time)).expectBody(key,equalTo(value)).build();
+        return response;
+    }
+}
---
+++ src/test/java/Practice2/FileReader.java
@@ -0,0 +1,29 @@
+package Practice2;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class FileReader {
+
+    public static String configReader(String key){
+        Properties prop=new Properties();
+        try{
+            FileInputStream files=new FileInputStream("src/test/resources/Config.properties");
+            prop.load(files);
+        }catch (Exception e){
+            throw new RuntimeException("unable to read the cofigFile"+e.getMessage());
+        }
+        return prop.getProperty(key);
+    }
+    public static JSONObject jsonReader(String key) throws IOException, ParseException {
+        JSONParser parser=new JSONParser();
+        JSONObject json= (JSONObject) parser.parse(new java.io.FileReader("src/test/resources/testData/testSet1.json"));
+        return (JSONObject) json.get(key);
+    }
+}
